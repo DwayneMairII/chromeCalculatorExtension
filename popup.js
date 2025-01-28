767ea0ebe5e514888604c1e27b1620d14b61217b
Dwayne Mair II <168599071+DwayneMairII@users.noreply.github.com>
2025-01-27 22:58:36 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,20 +1,8 @@
-// Function to load the HTML content from 'index.html'
-function loadHTML() {
-    fetch('calculator/index.html')  // Fetch the content from 'index.html'
-        .then(response => {
-            if (response.ok) {
-                return response.text();  // Get the content as text
-            }
-            throw new Error('Failed to load HTML');
-        })
-        .then(html => {
-            // Get the container where the HTML content will go
-            document.getElementById('container').innerHTML = html;
-        })
-        .catch(error => {
-            console.error('Error loading HTML:', error);
-        });
-}
-
-// Load the HTML content as soon as the page is loaded
-window.onload = loadHTML;
+chrome.windows.create({
+    url: 'calculator/index.html',
+    type: 'popup',
+    width: 800,
+    height: 600,
+}, function(window) {
+    console.log('Window created:', window);
+});
